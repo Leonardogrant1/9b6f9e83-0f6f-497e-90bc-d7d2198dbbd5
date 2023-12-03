9d38e54ce5be47f3a8425e6b8d4ba5919e82be47
Leonardogrant1 <LeonardoGranetto@web.de>
2023-12-03 20:19:13 +0100

    feat: import resolving

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@lib": fileURLToPath(new URL("./lib", import.meta.url)),
+      "@src": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   plugins: [react()],
-})
+});
